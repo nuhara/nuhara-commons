b1cbde02a1022d2d176c46ae98fac22482d45e3f
Alistair A. Israel <aisrael@gmail.com>
2011-08-31 21:50:07 +0800

    Added LocalTime & LocalTimeTest

---
+++ src/main/java/com/nuhara/commons/datetime/LocalTime.java
@@ -0,0 +1,186 @@
+/**
+ * Copyright (c) 2011 Nuhara, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Created Aug 31, 2011
+ */
+package com.nuhara.commons.datetime;
+
+import java.io.Serializable;
+
+/**
+ * An 'abstract' time of day (in 24-hour format), with no date or time zone information. Provides up to millisecond
+ * resolution.
+ *
+ * @author Alistair A. Israel
+ * @since 0.1
+ */
+public class LocalTime implements Serializable, Comparable<LocalTime> {
+
+    /**
+     * Computed serial version UID
+     */
+    private static final long serialVersionUID = 4057426100116054081L;
+
+    private final int hour;
+
+    private final int minute;
+
+    private final int second;
+
+    private final int millis;
+
+    /**
+     * @param hour
+     *        the hour of day, which can be from 0 (midnight past) to 24 (next midnight)
+     * @param minute
+     *        the minute
+     */
+    public LocalTime(final int hour, final int minute) {
+        this(hour, minute, 0, 0);
+    }
+
+    /**
+     * @param hour
+     *        the hour of day
+     * @param minute
+     *        the minute
+     * @param second
+     *        the second
+     */
+    public LocalTime(final int hour, final int minute, final int second) {
+        this(hour, minute, second, 0);
+    }
+
+    /**
+     * @param hour
+     *        the hour of day
+     * @param minute
+     *        the minute
+     * @param second
+     *        the second
+     * @param millis
+     *        the milliseconds
+     */
+    // SUPPRESS CHECKSTYLE CyclomaticComplexity
+    public LocalTime(final int hour, final int minute, final int second, final int millis) {
+        if (hour < 0 || hour > 24) {
+            throw new IllegalArgumentException("Invalid hour: " + hour);
+        }
+        if (hour == 24 && !(minute == 0 && second == 0 && millis == 0)) {
+            throw new IllegalArgumentException("Can only represent up to 24:00:00.000!");
+        }
+        if (minute < 0 || minute >= 60) {
+            throw new IllegalArgumentException("Invalid minute: " + minute);
+        }
+        if (second < 0 || second >= 60) {
+            throw new IllegalArgumentException("Invalid second: " + second);
+        }
+        if (millis < 0 || millis >= 1000) {
+            throw new IllegalArgumentException("Invalid millisecond: " + millis);
+        }
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+        this.millis = millis;
+    }
+
+    /**
+     * @return the hour
+     */
+    public final int getHour() {
+        return hour;
+    }
+
+    /**
+     * @return the minute
+     */
+    public final int getMinute() {
+        return minute;
+    }
+
+    /**
+     * @return the second
+     */
+    public final int getSecond() {
+        return second;
+    }
+
+    /**
+     * @return the millis
+     */
+    public final int getMillis() {
+        return millis;
+    }
+
+    /**
+     * @return the number of milliseconds past midnight this time object represents
+     */
+    public final int toMillisPastMidnight() {
+        return ((((hour * 60) + minute) * 60) + second) * 1000 + millis;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see java.lang.Object#hashCode()
+     */
+    // SUPPRESS CHECKSTYLE DesignForExtension
+    @Override
+    public int hashCode() {
+        return toMillisPastMidnight();
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    // SUPPRESS CHECKSTYLE DesignForExtension
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final LocalTime other = (LocalTime) obj;
+        return this.hour == other.hour && this.minute == other.minute && this.second == other.second
+                && this.millis == other.millis;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see java.lang.Comparable#compareTo(java.lang.Object)
+     */
+    // SUPPRESS CHECKSTYLE DesignForExtension
+    @Override
+    public int compareTo(final LocalTime other) {
+        final int ms = toMillisPastMidnight();
+        final int otherMs = other.toMillisPastMidnight();
+        if (ms == otherMs) {
+            return 0;
+        } else if (ms < otherMs) {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+
+}
---
+++ src/test/java/com/nuhara/commons/datetime/LocalTimeTest.java
@@ -0,0 +1,149 @@
+/**
+ * Copyright (c) 2011 Nuhara, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Created Aug 31, 2011
+ */
+package com.nuhara.commons.datetime;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+/**
+ * @author Alistair A. Israel
+ */
+public final class LocalTimeTest {
+
+    private static final LocalTime LAST_MIDNIGHT = new LocalTime(0, 0);
+
+    private static final LocalTime NOON = new LocalTime(12, 0);
+
+    private static final LocalTime NEXT_MIDNIGHT = new LocalTime(24, 0);
+
+    private static final LocalTime[] TIMES = { LAST_MIDNIGHT, new LocalTime(1, 23, 45, 678), NOON, NEXT_MIDNIGHT };
+
+    /**
+     * Test construction with invalid parameters
+     */
+    @Test
+    public void testInvalidTimes() {
+        final int[][] aargs = {
+            { -1, 0 },
+            { 25, 0 },
+            { 0, -1 },
+            { 0, 60 },
+            { 0, 0, -1 },
+            { 0, 0, 60 },
+        };
+        final String[] messages = {
+            "Invalid hour: -1",
+            "Invalid hour: 25",
+            "Invalid minute: -1",
+            "Invalid minute: 60",
+            "Invalid second: -1",
+            "Invalid second: 60",
+        };
+        for (int i = 0; i < aargs.length; ++i) {
+            final int[] args = aargs[i];
+            try {
+                @SuppressWarnings("unused")
+                final LocalTime time = constructUsing(args);
+                fail("No exception thrown for: " + Arrays.toString(args));
+            } catch (final IllegalArgumentException e) {
+                assertEquals(messages[i], e.getMessage());
+            }
+        }
+    }
+
+    /**
+     * @param args
+     *        the args
+     * @return LocalTime
+     */
+    private static LocalTime constructUsing(final int... args) {
+        switch (args.length) {
+        case 1:
+            fail("Not enough arguments: " + Arrays.toString(args));
+            break;
+        case 2:
+            return new LocalTime(args[0], args[1]);
+        case 3:
+            return new LocalTime(args[0], args[1], args[2]);
+        case 4:
+            return new LocalTime(args[0], args[1], args[2], args[3]);
+        default:
+            fail("Too many arguments: " + Arrays.toString(args));
+        }
+        return null;
+    }
+
+    /**
+     * Test for {@link LocalTime#toMillisPastMidnight()}.
+     */
+    @Test
+    public void testToMillisPastMidnight() {
+        final int[] expected = { 0, 5025678 };
+        for (int i = 0; i < expected.length; ++i) {
+            final LocalTime time = TIMES[i];
+            assertEquals(expected[i], time.toMillisPastMidnight());
+        }
+    }
+
+    /**
+     * Test for {@link LocalTime#hashCode()}
+     */
+    @Test
+    public void testHashCode() {
+        final int[] expected = { 0, 5025678, 43200000, 86400000 };
+        for (int i = 0; i < expected.length; ++i) {
+            final LocalTime time = TIMES[i];
+            assertEquals(expected[i], time.hashCode());
+        }
+    }
+
+    /**
+     * Test for {@link LocalTime#equals(java.lang.Object)}.
+     */
+    @Test
+    public void testEquals() {
+        final LocalTime time1 = TIMES[1];
+        final Object time2 = new LocalTime(1, 23, 45, 678);
+        assertTrue(time1.equals(time2));
+        assertTrue(time2.equals(time1));
+        assertEquals(time1, time2);
+        assertEquals(time2, time1);
+
+        assertFalse(time1.equals(NOON));
+    }
+
+    /**
+     * Test for {@link LocalTime#compareTo(LocalTime)}.
+     */
+    @Test
+    public void testCompareTo() {
+        final LocalTime time1 = TIMES[1];
+        final LocalTime time2 = new LocalTime(1, 23, 45, 678);
+        assertEquals(0, time1.compareTo(time2));
+        assertEquals(0, time2.compareTo(time1));
+
+        assertEquals(-1, LAST_MIDNIGHT.compareTo(NOON));
+        assertEquals(1, NEXT_MIDNIGHT.compareTo(NOON));
+    }
+}
